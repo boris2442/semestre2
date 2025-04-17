5f976d0ed9d24cce485fa7dacdb8b546cf7328c1
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-17 23:07:46 +0200

    text

--- delete.php
+++ delete.php
@@ -1,50 +1,23 @@
 <?php
-// require "database/database.php";
-
-// if (isset($_GET['id'])) {
-//     $id = $_GET['id'];
-
-//     $sql = "DELETE FROM `produits` WHERE id = :id";
-//     $requete = $db->prepare($sql);
-//     $requete->bindValue(":id", $id);
-//     $requete->execute();
-
-
-//     header("Location: index_produit.php");
-//     exit(); 
-// } else {
-//     echo "ID non retrouvé";
-// }
-
-
-
-
-
-
-
-
-
-
-
 
 require "database/database.php";
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Supprimer les enregistrements dépendants dans la table nomenclature
-    $sql1 = "DELETE FROM `nomenclature` WHERE id_produit = :id";
+   
+    $sql1 = "DELETE FROM `nomenclature` WHERE id= :id";
     $requete1 = $db->prepare($sql1);
     $requete1->bindValue(":id", $id);
     $requete1->execute();
 
-    // Supprimer le produit
+
     $sql2 = "DELETE FROM `produits` WHERE id = :id";
     $requete2 = $db->prepare($sql2);
     $requete2->bindValue(":id", $id);
     $requete2->execute();
 
-    // Rediriger sans echo avant
+  
     header("Location: index_produit.php");
     exit(); 
 } else {
